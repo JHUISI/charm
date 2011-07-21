348ac463ad23836991cd5989acc86ded264acf89
Gary Belvin <gary.belvin@gmail.com>
2011-07-21 19:32:07 +0000

    auto doc generator

--- doc/source/autoschemes.py
+++ doc/source/autoschemes.py
@@ -4,6 +4,9 @@ Script to automatically generate documentation stubs for schemes
 Author: Gary Belvin
 '''
 import os, re
+
+rstschemesdir = "schemes"
+
 def find_modules(path="."):
     modules = set()
     for filename in os.listdir(path):
@@ -17,32 +20,44 @@ def find_modules(path="."):
 
 def gen_toc(modules):
    out = ""
-   dir = "schemes"
    for m in modules:
-       out += dir + "/" + m + '\n'
+       out += rstschemesdir + "/" + m + '\n'
    return out
 
 def gen_doc_stub(module):
-   out = \
-   """%s
-      =========================================
-      .. todo::
-      Document %s
-      
-      .. automodule:: %s
-         :show-inheritance:
-         :synopsis:
-         :members:
-      """ %(module, module, module)
+   out ="""
+%s
+=========================================
+.. todo::
+   Document %s
+
+.. automodule:: %s
+    :show-inheritance:
+    :synopsis:
+    :members:
+""" %(module, module, module)
    return out
  
 
 if __name__ == "__main__": 
    mods = find_modules("../../schemes/")
-   print("Modules  =>", mods)
-   print("TOC      =>",gen_toc(mods))
-   print("module   =>", mods[1])
-   print(gen_doc_stub(mods[1]))
+   #Create files for undocumented modules
+   for m in mods:
+       #only create stubs if the scheme hasn't already been documented
+       rstpath = rstschemesdir + '/' + m + ".rst"
+       if not os.path.isfile(rstpath):
+           with open(rstpath, mode='w',  encoding='utf-8') as f:
+               print("Writing new file ", rstpath)
+               f.write(gen_doc_stub(m))
+           
+   
+   #Add modules to main table of contents
+   
+   
+   #print("Modules  =>", mods)
+   #print("TOC      =>",gen_toc(mods))
+   #print("module   =>", mods[1])
+   #print(gen_doc_stub(mods[1]))
 
   
 
