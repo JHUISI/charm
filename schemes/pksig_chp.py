2720abfe61ce2dd66c5aaef95dc2b4dbbb79496c
J Ayo Akinyele <waldoayo@gmail.com>
2011-09-23 03:34:09 +0000

    chp update

--- schemes/pksig_chp.py
+++ schemes/pksig_chp.py
@@ -1,38 +1,57 @@
 """ TODO: Description of Scheme here.
 """
-
-from toolbox.pairinggroup import *
-from charm.engine.util import *
-from toolbox.PKSig import *
+from charm.pairing import *
+from toolbox.PKSig import PKSig
 
 debug = False
+
 class CHP(PKSig):
     def __init__(self, groupObj):
-        global group, H1, H2, H3
+        global group, H
         group = groupObj
         
     def setup(self):
-        H1 = lambda x: group.hash(('0', str(x)), G1)
-        H2 = lambda y: group.hash(('1', str(y)), G1)
-        H3 = lambda a,b: group.hash(('2', str(a), str(b)), ZR)
+        global H,H3
+        H = lambda prefix,x: group.H((str(prefix), str(x)), G1)
+        H3 = lambda a,b: group.H(('3', str(a), str(b)), ZR)
     
     def keygen(self):
         g2, alpha = group.random(G2), group.random(ZR)
         sk = alpha
-        pk = {'g2':g2, 'g2^a':g2 ** alpha}
+        pk = {'g2':g2, 'g2a':g2 ** alpha}
         return (pk, sk)
     
     def sign(self, pk, sk, M):
-        a = H1(M['t1'])
-        h = H2(M['t2'])
-        b = H3(M['t3'], M['str'])
+        a = H(1, M['t1'])
+        h = H(2, M['t2'])
+        b = H3(M['str'], M['t3'])
         sig = (a ** sk) * (h ** (sk * b))        
         return sig
     
     def verify(self, pk, M, sig):
-        a = H1(M['t1'])
-        h = H2(M['t2'])
-        b = H3(M['t3'], M['str'])
-        if pair(sig, pk['g2']) == (pair(a, pk['g2^a']) * pair(h, pk['g2^a'])):
+        a = H(1, M['t1'])
+        h = H(2, M['t2'])
+        b = H3(M['str'], M['t3'])
+        if pair(sig, pk['g2']) == (pair(a, pk['g2a']) * (pair(h, pk['g2a']) ** b)):
             return True
         return False
+
+if __name__ == "__main__":
+   
+   groupObj = pairing('../param/a.param')
+   chp = CHP(groupObj)
+   chp.setup()
+
+   (pk, sk) = chp.keygen()  
+   print("Keygen...")
+   print("pk =>", pk)
+   print("sk =>", sk)
+  
+   M = { 't1':'time_1', 't2':'time_2', 't3':'time_3', 'str':'this is the message'}
+   sig = chp.sign(pk, sk, M)
+   print("Signature...")
+   print("sig =>", sig)
+
+   assert chp.verify(pk, M, sig), "invalid signature!"
+   print("Verification successful!")
+   
