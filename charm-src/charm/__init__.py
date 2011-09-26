9db74748f653e974268bc4b293c0e9c12f36ad48
J Ayo Akinyele <waldoayo@gmail.com>
2011-09-26 14:54:54 +0000

    another update

--- charm-src/charm/__init__.py
+++ charm-src/charm/__init__.py
@@ -3,6 +3,7 @@ import platform
 
 install_system=platform.system()
 
+path_to_charm2 = None
 if install_system == 'Darwin':
    # get the path to site-packages for operating system
    path_to_charm = distutils.sysconfig.get_python_lib()
@@ -13,6 +14,7 @@ elif install_system == 'Linux':
    dist = platform.linux_distribution()[0]
    if dist == 'Ubuntu' or 'LinuxMint':
       path_to_charm = distutils.sysconfig.get_python_lib(1, 1, '/usr/local') + "/dist-packages"
+      path_to_charm2 = distutils.sysconfig.get_python_lib(1, 1, '/usr/local') + "/site-packages"
    elif dist == 'Fedora':
       path_to_charm = distutils.sysconfig.get_python_lib(1, 1, '/usr') + "/site-packages"
    #print("python path =>", path_to_charm)
@@ -20,6 +22,7 @@ else:
    print("Installing on", install_system)
    
 sys.path.append(path_to_charm + "/charm/")
+if path_to_charm2: sys.path.append(path_to_charm2 + "/charm/")
 # now python can easily find our modules
 # dependency for pairing, integer and ecc mods
 import charm.benchmark
