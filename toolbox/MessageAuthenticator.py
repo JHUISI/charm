edfe2ddac8f3a852119d287bf16cdd12c43baf03
Ian Miers <imichaelmiers@gmail.com>
2011-11-03 16:26:37 -0400

    renamed MacMessageAuthenticator.py

---
+++ toolbox/MessageAuthenticator.py
@@ -0,0 +1,42 @@
+import hmac 
+from hashlib import sha1 
+from charm.pairing import hash as extractor
+class MessageAuthenticator(object):
+    """ Abstraction for constructing and verifying authenticated messages 
+        
+    >>> from toolbox.pairinggroup import PairingGroup,GT
+    >>> groupObj = PairingGroup('../param/a.param')
+    >>> key = groupObj.random(GT)
+    >>> m = MessageAuthenticator(key)
+    >>> AuthenticatedMessage = m.mac('Hello World')
+    >>> m.verify(AuthenticatedMessage)
+    True
+    """
+    def __init__(self,key, alg = "HMAC_SHA1"):
+        """
+        Creates a message authenticator and verifier under the specified key
+        """
+        if alg != "HMAC_SHA1":
+            raise ValueError("Currently only HMAC_SHA1 is supportated as an algorithm")
+        self._algorithm = alg
+        self._key = sha1(b'Poor Mans Key Extractor'+extractor(key)).digest() # warning only valid in the random oracle
+    
+    def mac(self,msg):
+        """
+        authenticates a message 
+        """
+        return {
+                "alg": self._algorithm,
+                "msg": msg, 
+                "digest": hmac.new(self._key,bytes(msg,'utf-8'),digestmod=sha1).hexdigest()
+               }
+
+    def verify(self,msgAndDigest):
+        """
+        verifies the result returned by mac
+        """
+        if msgAndDigest['alg'] != self._algorithm:
+            raise ValueError()
+        expected = bytes(self.mac(msgAndDigest['msg'])['digest'],'utf-8')
+        recieved = bytes(msgAndDigest['digest'],'utf-8')
+        return sha1(expected).digest() == sha1(recieved).digest() # we compare the hash instead of the direct value to avoid a timing attack
