5072b34c1e432793202172db2f2f402c0f4a5473
Ian Miers <imichaelmiers@gmail.com>
2011-11-04 17:59:49 -0400

    added authenticated encryption and tests

---
+++ toolbox/AuthenticatedCryptoAbstraction.py
@@ -0,0 +1,14 @@
+from MessageAuthenticator import MessageAuthenticator
+from SymmetricCryptoAbstraction import SymmetricCryptoAbstraction
+
+class AuthenticatedCryptoAbstraction(SymmetricCryptoAbstraction):
+   def encrypt(self,msg):
+        mac = MessageAuthenticator(self._key) 
+        enc = super(AuthenticatedCryptoAbstraction,self).encrypt(msg)
+        return mac.mac(enc);
+   def decrypt(self,cipherText): 
+        mac = MessageAuthenticator(self._key) 
+        if not  mac.verify(cipherText):
+            raise ValueError("Invalid mac. Your data was tampered with or your key is wrong")
+        else:
+            return super(AuthenticatedCryptoAbstraction,self).decrypt(cipherText['msg'])
