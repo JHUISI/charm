fbf3b47f8b3ba2b3a8d3e9a8424630720f9b5cdf
Matt <matt@hell.isi.jhu.edu>
2012-01-17 18:33:59 -0500

    CL stuff

---
+++ auto_batch/codegen/CL/ind.py
@@ -0,0 +1,39 @@
+from toolbox.PKSig import PKSig
+from toolbox.pairinggroup import PairingGroup,ZR,G1,G2,pair
+import sys
+
+group = None
+debug = None
+bodyKey = 'Body'
+
+def __init__( groupObj ) : 
+	global group , debug 
+	group= groupObj 
+	debug= False 
+
+def run_Ind(verifyArgsDict, groupObjParam, verifyFuncArgs):
+	global group
+	global debug
+	group = groupObjParam
+
+	N = len(verifyArgsDict)
+	z = 0
+	incorrectIndices = []
+	__init__(group)
+
+	for z in range(0, N):
+		#for arg in verifyFuncArgs:
+			#if (group.ismember(verifyArgsDict[z][arg][bodyKey]) == False):
+				#sys.exit("ALERT:  Group membership check failed!!!!\n")
+
+		pass
+
+		( a , b , c )= verifyArgsDict[z]['sig'][bodyKey][ 'a' ] , verifyArgsDict[z]['sig'][bodyKey][ 'a_y' ] , verifyArgsDict[z]['sig'][bodyKey][ 'a_xy' ]
+		m= group.hash( verifyArgsDict[z]['M'][bodyKey] , ZR )
+		if pair( verifyArgsDict[z]['pk'][bodyKey][ 'Y' ] , a )== pair( verifyArgsDict[z]['pk'][bodyKey][ 'g' ] , b ) and( pair( verifyArgsDict[z]['pk'][bodyKey][ 'X' ] , a ) *( pair( verifyArgsDict[z]['pk'][bodyKey][ 'X' ] , b ) ** m ) )== pair( verifyArgsDict[z]['pk'][bodyKey][ 'g' ] , c ) :
+			pass
+		else:
+			if z not in incorrectIndices:
+				incorrectIndices.append(z)
+
+	return incorrectIndices
