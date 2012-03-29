6aa7db4ef3724c533f252be2936abbba8c6ff3a8
Matt <matt@hell.isi.jhu.edu>
2012-03-29 17:22:40 -0400

    more on codegen

--- auto_outsrc/parser/codegen.py
+++ auto_outsrc/parser/codegen.py
@@ -6,6 +6,16 @@ assignInfo = None
 setupFile = None
 transformFile = None
 decOutFile = None
+currentFuncName = NONE_FUNC_NAME
+numTabsIn = 1
+
+def writeCurrentNumTabsIn(outputFile):
+    outputString = ""
+
+    for numTabs in range(0, numTabsIn):
+        outputString += "\t"
+
+    outputFile.write(outputString)
 
 def addImportLines():
     global setupFile, transformFile, decOutFile
@@ -56,7 +66,7 @@ def writeFunctionEnd_Python(outputFile, functionName):
         outputVariablesString = outputVariablesString[0:(len(outputVariablesString) - len(", "))]
         outputVariablesString += ")"
 
-    outputString += "\treturn"
+    outputString += "\treturn "
     outputString += outputVariablesString
     outputString += "\n\n"
 
@@ -95,9 +105,9 @@ def writeFunctionDecl_CPP(outputFile, functionName):
 def writeFunctionDecl(functionName):
     global setupFile, transformFile, decOutFile
 
-    if (functionName == transformFunctionName):
+    if (currentFuncName == transformFunctionName):
         writeFunctionDecl_Python(transformFile, functionName)
-    elif (functionName == decOutFunctionName):
+    elif (currentFuncName == decOutFunctionName):
         writeFunctionDecl_CPP(decOutFile, functionName)
     else:
         writeFunctionDecl_Python(setupFile, functionName)
@@ -105,15 +115,78 @@ def writeFunctionDecl(functionName):
 def writeFunctionEnd(functionName):
     global setupFile, transformFile, decOutFile
 
-    if (functionName == transformFunctionName):
+    if (currentFuncName == transformFunctionName):
         writeFunctionEnd_Python(transformFile, functionName)
-    elif (functionName == decOutFunctionName):
+    elif (currentFuncName == decOutFunctionName):
         writeFunctionEnd_CPP(decOutFile, functionName)
     else:
         writeFunctionEnd_Python(setupFile, functionName)
 
+def isForLoopStart(binNode):
+    if (binNode.type == ops.FOR):
+        #print(binNode)
+        pass
+
+def isForLoopEnd(binNode):
+    return False
+
+def writeForLoopEnd():
+    return
+
+def isAssignStmt(binNode):
+    if (binNode.type == ops.EQ):
+        return True
+
+    return False
+
+def getAssignStmtAsString(node):
+    if (node.type == ops.ATTR):
+        return node.attr
+    elif (node.type == ops.ADD):
+        leftString = getAssignStmtAsString(node.left)
+        rightString = getAssignStmtAsString(node.right)
+        return leftString + " + " + rightString
+    elif (node.type == ops.SUB):
+        leftString = getAssignStmtAsString(node.left)
+        rightString = getAssignStmtAsString(node.right)
+        return leftString + " - " + rightString
+    elif (node.type == ops.MUL):
+        leftString = getAssignStmtAsString(node.left)
+        rightString = getAssignStmtAsString(node.right)
+        return leftString + " * " + rightString
+    elif (node.type == ops.DIV):
+        leftString = getAssignStmtAsString(node.left)
+        rightString = getAssignStmtAsString(node.right)
+        return leftString + " / " + rightString
+    elif (node.type == ops.EXP):
+        leftString = getAssignStmtAsString(node.left)
+        rightString = getAssignStmtAsString(node.right)
+        return leftString + " ** " + rightString
+
+    return "" #replace with sys.exit
+
+def writeAssignStmt_Python(outputFile, binNode):
+    writeCurrentNumTabsIn(outputFile)
+
+    outputString = ""
+    outputString += binNode.left.attr
+    outputString += " = "
+
+    outputString += getAssignStmtAsString(binNode.right)
+    
+    outputString += "\n"
+    outputFile.write(outputString)
+
+def writeAssignStmt(binNode):
+    if (currentFuncName == transformFunctionName):
+        writeAssignStmt_Python(transformFile, binNode)
+    elif (currentFuncName == decOutFunctionName):
+        writeAssignStmt_CPP(decOutFile, binNode)
+    else:
+        writeAssignStmt_Python(setupFile, binNode)
+
 def writeSDLToFiles(astNodes):
-    currentFuncName = NONE_FUNC_NAME
+    global currentFuncName, numTabsIn
 
     for astNode in astNodes:
         if (isFunctionStart(astNode) == True):
@@ -122,6 +195,18 @@ def writeSDLToFiles(astNodes):
         elif (isFunctionEnd(astNode) == True):
             writeFunctionEnd(currentFuncName)
             currentFuncName = NONE_FUNC_NAME
+
+        if (currentFuncName == NONE_FUNC_NAME):
+            continue
+
+        if (isForLoopStart(astNode) == True):
+            writeForLoopDecl(astNode)
+            numTabsIn += 1
+        elif (isForLoopEnd(astNode) == True):
+            writeForLoopEnd()
+            numTabsIn -= 1
+        elif (isAssignStmt(astNode) == True):
+            writeAssignStmt(astNode)
 
 def main(SDL_Scheme):
     global setupFile, transformFile, decOutFile, assignInfo
