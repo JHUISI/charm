b06d09a5c9d8c7964ed2297c1eeaf2e37bd78cc3
Matt <matt@1025213512.rutm1.ra.johnshopkins.edu>
2012-04-05 14:56:44 -0400

    codegen

---
+++ auto_outsrc/parser/IfElse.py
@@ -0,0 +1,31 @@
+import sys
+from SDLang import *
+
+class IfElse:
+    def __init__(self):
+        self.conditionalAsNode = None
+        self.conditionalAsString = None
+        self.thenNodesAsBinNodes_List = None
+        self.thenNodesAsVarInfoObjs_List = None
+        self.elseNodesAsBinNodes_List = None
+        self.elseNodesAsVarInfoObjs_List = None
+
+    def getConditionalAsNode(self):
+        return self.conditionalAsNode
+
+    def getConditionalAsString(self):
+        return self.conditionalAsString
+
+    def getThenNodesAsBinNodes_List(self):
+        return self.thenNodesAsBinNodes_List
+
+    def getThenNodesAsVarInfoObjs_List(self):
+        return self.thenNodesAsVarInfoObjs_List
+
+    def getElseNodesAsBinNodes_List(self):
+        return self.elseNodesAsBinNodes_List
+
+    def getElseNodesAsVarInfoObjs_List(self):
+        return self.elseNodesAsVarInfoObjs_List
+
+    def setIfElseStruct(node):
