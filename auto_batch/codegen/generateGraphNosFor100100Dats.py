aaa1fe91f23e50cdc8e6bce0e1deab2df531eea8
Matt <matt@1025264253.rude1.ra.johnshopkins.edu>
2012-05-02 16:24:34 -0400

    benchmarks

--- auto_batch/codegen/generateGraphNosFor100100Dats.py
+++ auto_batch/codegen/generateGraphNosFor100100Dats.py
@@ -1,4 +1,4 @@
-import sys
+import sys, numpy
 
 numIterations = 100
 numSignatures = 100
@@ -8,11 +8,16 @@ runningTimesSuffix = ".dat"
 runningTimeSchemes = ["BBS", "BLS", "Boyen", "CHCH", "ChCh_Hess", "CHP", "CL", "CYH", "HESS", "HW_Different", "VRF", "WATERS", "Waters09"]
 runningTimeTypes = ["batcher", "codegen"]
 
+runningTimesOutputFileSuffix = "_Running_Times"
+
 benchmarkPrefix = "CCS2012_Benchmarks_"
 benchmarkSuffix = ".dat"
 benchmarkTypes = ["bat", "ind"]
 #benchmarkSchemes = ["BOYEN", "CHCH_HESS", "CL", "CDH/CDH", "HW_DIFF", "VRF", "WATERS09"]
 benchmarkSchemes = ["BOYEN", "CHCH_HESS", "CL", "CDH/CDH", "HW_DIFF"]
+
+inputDelimiter = ","
+outputDelimiter = " : "
 
 def buildIOFileNames():
 	runningInNames = {}
@@ -30,14 +35,40 @@ def buildIOFileNames():
 
 	return (runningInNames, benchmarkInNames)
 
-def getRunningTimesOutput(runningInNames):
-	retDict = {}
+def getRunningTimesOutput(runningInNames, prefixName):
+	outputFile = open(prefixName + runningTimesOutputFileSuffix + runningTimesSuffix, 'w')
+	outputString = ""
 
-	return retDict
+	for scheme in runningInNames:
+		for type in runningInNames[scheme]:
+			inputFileName = runningInNames[scheme][type]
+			inputFile = open(inputFileName, 'r').readlines()
+			if (len(inputFile) != 1):
+				sys.exit("One of the running time input files did not have only one line.")
+
+			inputSplit = inputFile[0].split(inputDelimiter)
+			if (len(inputSplit) != (numIterations + 1) ):
+				sys.exit("One of the running time input files did not have numIterations+1 split entries.")
+
+			lastValue = inputSplit.pop()
+			if (lastValue != ""):
+				sys.exit("One of the running time input files had a last element that was non-NULL.")
+
+			inputAsFloats = []
+			for currentInput in inputSplit:
+				inputAsFloats.append(float(currentInput))
+
+			if (len(inputAsFloats) != numIterations):
+				sys.exit("inputAsFloats for running times was not of length numIterations.")
+
+			outputString += scheme + outputDelimiter + type + outputDelimiter + str(numpy.mean(inputAsFloats, dtype=numpy.float64)) + outputDelimiter + str(numpy.std(inputAsFloats, dtype=numpy.float64)) + "\n"
+
+	outputFile.write(outputString)
+	outputFile.close()
 
 def main(prefixName):
 	(runningInNames, benchmarkInNames) = buildIOFileNames()
-	runningTimesOutputDict = getRunningTimesOutput(runningInNames)
+	runningTimesOutputDict = getRunningTimesOutput(runningInNames, prefixName)
 
 if __name__ == '__main__':
 	if ( (len(sys.argv) != 2) or (sys.argv[1] == "-help") or (sys.argv[1] == "--help") ):
