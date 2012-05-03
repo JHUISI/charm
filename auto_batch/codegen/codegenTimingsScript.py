e720e7cc15dcbad5a64f38bb21dae525a1557fad
Joseph Akinyele <jakinye3@goose.isi.jhu.edu>
2012-05-02 23:12:11 -0400

    update

--- auto_batch/codegen/codegenTimingsScript.py
+++ auto_batch/codegen/codegenTimingsScript.py
@@ -19,51 +19,51 @@ def buildSchemesDetails():
 
 	# Bls
 	schemesDetails["BLS"]["codegen"] = ["BLS/pksig_bls04.py", "BLS/batchOutput_BLS", "garbageValue", "tmp/blsIND.py", "tmp/blsBAT.py", "tmp/blsIND.py"] 
-	schemesDetails["BLS"]["batcher"] = ["batchverify.py", "../tests/bls.bv", "-b", "-c", "-p"]
+	schemesDetails["BLS"]["batcher"] = ["batchverify.py", "../tests/bls.bv", "-p"]
 
 	# Chp
 	schemesDetails["CHP"]["codegen"] = ["CHP/pksig_chp.py", "CHP/batchOutputCHP", "garbageValue", "tmp/chpIND.py", "tmp/chpBAT.py", "tmp/chpIND.py"] 
-	schemesDetails["CHP"]["batcher"] = ["batchverify.py", "../tests/chp.bv", "-b", "-c", "-p"]
+	schemesDetails["CHP"]["batcher"] = ["batchverify.py", "../tests/chp.bv", "-p"]
 
 	# Hess
 	schemesDetails["HESS"]["codegen"] = ["HESS/pksig_hess.py", "HESS/batchOutputHess", "garbageValue", "tmp/hessIND.py", "tmp/hessBAT.py", "tmp/hessIND.py"] 
-	schemesDetails["HESS"]["batcher"] = ["batchverify.py", "../tests/hess.bv", "-b", "-c", "-p"]
+	schemesDetails["HESS"]["batcher"] = ["batchverify.py", "../tests/hess.bv", "-p"]
 
 	# ChCh
 	schemesDetails["CHCH"]["codegen"] = ["CHCH/pksig_chch.py", "CHCH/batchOutputCHCH", "garbageValue", "tmp/chchIND.py", "tmp/chchBAT.py", "tmp/chchIND.py"] 
-	schemesDetails["CHCH"]["batcher"] = ["batchverify.py", "../tests/chch.bv", "-b", "-c", "-p"]
+	schemesDetails["CHCH"]["batcher"] = ["batchverify.py", "../tests/chch.bv", "-p"]
 
 	# Waters05
 	schemesDetails["WATERS"]["codegen"] = ["WATERS/pksig_waters.py", "WATERS/batchOutput", "garbageValue", "tmp/watersIND.py", "tmp/watersBAT.py", "tmp/watersIND.py"] 
-	schemesDetails["WATERS"]["batcher"] = ["batchverify.py", "../tests/waters.bv", "-b", "-c", "-p"]
+	schemesDetails["WATERS"]["batcher"] = ["batchverify.py", "../tests/waters.bv", "-p"]
 
 	# Cyh
 	schemesDetails["CYH"]["codegen"] = ["CYH/pksig_cyh.py", "CYH/batchOutputCYH", "garbageValue", "tmp/cyhIND.py", "tmp/cyhBAT.py", "tmp/cyhIND.py"] 
-	schemesDetails["CYH"]["batcher"] = ["batchverify.py", "../tests/cyh.bv", "-b", "-c", "-p"]
+	schemesDetails["CYH"]["batcher"] = ["batchverify.py", "../tests/cyh.bv", "-p"]
 
 	# Boyen
 	schemesDetails["Boyen"]["codegen"] = ["BOYEN/pksig_boyen.py.ORIGINAL.py", "BOYEN/batchOutputBoyen", "garbageValue", "tmp/boyenIND.py", "tmp/boyenBAT.py", "tmp/boyenIND.py"] 
-	schemesDetails["Boyen"]["batcher"] = ["batchverify.py", "../tests/boyen.bv", "-b", "-c", "-p"]
+	schemesDetails["Boyen"]["batcher"] = ["batchverify.py", "../tests/boyen.bv", "-p"]
 
         # BBS
 	schemesDetails["BBS"]["codegen"] = ["BBS/groupsig_bgls04_var.py", "BBS/batchOutput_AFTERAYOMODS", "garbageValue", "tmp/bbsIND.py", "tmp/bbsBAT.py", "tmp/bbsIND.py"] 
-	schemesDetails["BBS"]["batcher"] = ["batchverify.py", "../tests/bbs.bv", "-b", "-c", "-p"]
+	schemesDetails["BBS"]["batcher"] = ["batchverify.py", "../tests/bbs.bv", "-p"]
 	
 	# Chch + Hess
 	schemesDetails["ChCh_Hess"]["codegen"] = ["CHCH_HESS/pksig_case21.py", "CHCH_HESS/batchOutput", "garbageValue", "tmp/chchhessIND.py", "tmp/chchhessBAT.py", "tmp/chchhessVER.py"]
-	schemesDetails["ChCh_Hess"]["batcher"] = ["batchverify.py", "../tests/case2.bv", "-b", "-c", "-p"]
+	schemesDetails["ChCh_Hess"]["batcher"] = ["batchverify.py", "../tests/case2.bv", "-p"]
 
 	# VRF
 	schemesDetails["VRF"]["codegen"] = ["VRF/pk_vrf.py", "VRF/batchOutput", "garbageValue", "tmp/vrfIND.py", "tmp/vrfBAT.py", "tmp/vrfVER.py"]
-	schemesDetails["VRF"]["batcher"] = ["batchverify.py", "../tests/vrf.bv", "-b", "-c", "-p"]
+	schemesDetails["VRF"]["batcher"] = ["batchverify.py", "../tests/vrf.bv", "-p"]
 
 	# Cl
 	schemesDetails["CL"]["codegen"] = ["CL/pksig_cl04.py", "CL/batchOutput", "garbageValue", "tmp/clIND.py", "tmp/clBAT.py", "tmp/clVER.py"]
-	schemesDetails["CL"]["batcher"] = ["batchverify.py", "../tests/case1.bv", "-b", "-c", "-p"]
+	schemesDetails["CL"]["batcher"] = ["batchverify.py", "../tests/case1.bv", "-p"]
 
 	# HW - diff signers
 	schemesDetails["HW_Different"]["codegen"] = ["HW_DIFF/pksig_hw.py", "HW_DIFF/batchOutput", "gargabeValue", "tmp/hwdiffIND.py", "tmp/hwdiffBAT.py", "tmp/hwdiffVER.py"] 
-	schemesDetails["HW_Different"]["batcher"] = ["batchverify.py", "../tests/hw-diff.bv", "-b", "-c", "-p"]
+	schemesDetails["HW_Different"]["batcher"] = ["batchverify.py", "../tests/hw-diff.bv", "-p"]
 
 	# Waters09
 	schemesDetails["Waters09"]["codegen"] = ["WATERS09/pksig_waters09_mod.py", "WATERS09/batchOutput", "garbageValue", "tmp/waters09IND.py", "tmp/waters09BAT.py", "tmp/waters09VER.py"]
