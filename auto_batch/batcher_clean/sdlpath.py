0fd061f7ec6dfde63a67128afd86b90d18ba10f1
J Ayo Akinyele <jakinye3@jhu.edu>
2012-10-08 18:41:37 -0400

    updates

---
+++ auto_batch/batcher_clean/sdlpath.py
@@ -0,0 +1,10 @@
+import sys, time
+
+dir_above = '../'
+sdl_pkg   = '../sdlparser'
+
+if dir_above not in sys.path:
+   sys.path.append(dir_above)
+if sdl_pkg not in sys.path:
+   sys.path.append(sdl_pkg)
+
