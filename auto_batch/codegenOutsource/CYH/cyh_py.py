f6a10ef4a6fdd38bd6de1a38299a1a634cfa0bfa
Matt <matt@Matts-MacBook-Pro.local>
2012-10-24 21:06:37 -0400

    test

--- auto_batch/codegenOutsource/CYH/cyh_py.py
+++ auto_batch/codegenOutsource/CYH/cyh_py.py
@@ -1,18 +1,16 @@
 from charm.toolbox.pairinggroup import *
 from charm.core.engine.util import *
-#from charm.core.math.integer import randomBits
+from charm.core.math.integer import randomBits
 
 group = None
 
 N = 2
 
-l = 3
+l = 2
 
 secparam = 80
 
-Lt = {}
 dotProd = {}
-pkList = {}
 h = {}
 l = {}
 
@@ -32,9 +30,8 @@ def concat(ID_List):
     L = ""
     l = len(ID_List)
     for y in range(0, l):
-        L = L + ID_List[y] + ":"
+        L = (L + ID_List[y])
     output = L
-    print(L)
     return output
 
 def keygen(alpha, ID):
@@ -46,58 +43,118 @@ def keygen(alpha, ID):
     return output
 
 def sign(ID, pk, sk, L, M):
-    """
-    global Lt
     global dotProd
-    global pkList
     global h
-    """
+
     h = {}
     u = {}
-    pkList = {}
+    pklist = {}
 
     input = [ID, pk, sk, L, M]
     Lt = concat(L)
     for i in range(0, l):
         if ( ( (ID) != (L[i]) ) ):
             u[i] = group.random(G1)
-            h[i] = group.hash((M, (Lt, u[i])), ZR)
+            h[i] = group.hash((M, Lt, u[i]), ZR)
         else:
             s = i
     r = group.random(ZR)
     for y in range(0, l):
-        pkList[y] = group.hash(L[y], G1)
+        pklist[y] = group.hash(L[y], G1)
     dotProd = 1
     for i in range(0, l):
         if ( ( (ID) != (L[i]) ) ):
-            dotProd = (dotProd * (u[i] * (pkList[i] ** h[i])))
+            dotProd = (dotProd * (u[i] * (pklist[i] ** h[i])))
     u[s] = ((pk ** r) * (dotProd ** -1))
-    h[s] = group.hash((M, (Lt, u[s])), ZR)
+    h[s] = group.hash((M, Lt, u[s]), ZR)
     S = (sk ** (h[s] + r))
-    output = (u, S)
+    output = (Lt, pklist, u, S)
     return output
 
-def verify(P, g, L, M, u, S):
-    """ 
-    global Lt
+def verify(Lt, pklist, P, g, M, u, S):
     global dotProd
-    global pkList
     global h
-    """
 
-    input = [P, g, L, M, u, S]
-    Lt = concat(L)
-
+    input = [Lt, pklist, P, g, M, u, S]
     for y in range(0, l):
-        h[y] = group.hash((M, (Lt, u[y])), ZR)
-        pkList[y] = group.hash(L[y], G1)
+        h[y] = group.hash((M, Lt, u[y]), ZR)
     dotProd = 1
     for y in range(0, l):
-        dotProd = (dotProd * (u[y] * (pkList[y] ** h[y])))
+        dotProd = (dotProd * (u[y] * (pklist[y] ** h[y])))
     if ( ( (pair(dotProd, P)) == (pair(S, g)) ) ):
         output = True
     else:
         output = False
+        return output
+    return output
+
+def membership(P, Slist, g, pklist, ulist):
+
+    input = [P, Slist, g, pklist, ulist]
+    if ( ( (group.ismember(P)) == (False) ) ):
+        output = False
+        return output
+    if ( ( (group.ismember(Slist)) == (False) ) ):
+        output = False
+        return output
+    if ( ( (group.ismember(g)) == (False) ) ):
+        output = False
+        return output
+    if ( ( (group.ismember(pklist)) == (False) ) ):
+        output = False
+        return output
+    if ( ( (group.ismember(ulist)) == (False) ) ):
+        output = False
+        return output
+    output = True
+    return output
+
+def dividenconquer(delta, startSigNum, endSigNum, incorrectIndices, dotBCache, dotCCache, P, g):
+
+    input = [delta, startSigNum, endSigNum, incorrectIndices, dotBCache, dotCCache, P, g]
+    dotBLoopVal = 1
+    dotCLoopVal = 1
+    for z in range(startSigNum, endSigNum):
+        dotBLoopVal = (dotBLoopVal * dotBCache[z])
+        dotCLoopVal = (dotCLoopVal * dotCCache[z])
+    if ( ( (pair(dotBLoopVal, P)) == (pair(dotCLoopVal, g)) ) ):
+        return
+    else:
+        midwayFloat = ((endSigNum - startSigNum) / 2)
+        midway = int(midwayFloat)
+    if ( ( (midway) == (0) ) ):
+        incorrectIndices.append(startSigNum)
+        output = None
+    else:
+        midSigNum = (startSigNum + midway)
+        dividenconquer(delta, startSigNum, midway, incorrectIndices, dotBCache, dotCCache, P, g)
+        dividenconquer(delta, midSigNum, endSigNum, incorrectIndices, dotBCache, dotCCache, P, g)
+    output = None
+
+def batchverify(Lt, Mlist, P, Slist, g, pklist, ulist, incorrectIndices):
+    global h
+
+    dotCCache = {}
+    delta = {}
+    dotBCache = {}
+
+    input = [Lt, Mlist, P, Slist, g, pklist, ulist, incorrectIndices]
+    for z in range(0, N):
+        delta[z] = SmallExp(secparam)
+    if ( ( (membership(P, Slist, g, pklist, ulist)) == (False) ) ):
+        output = False
+        return output
+    for z in range(0, N):
+        dotALoopVal = 1
+        for y in range(0, l):
+            h = group.hash((Mlist[y], Lt, ulist[y]), ZR)
+            print("y = ", y)
+            print("z = ", z)
+            dotALoopVal = (dotALoopVal * ((ulist[z][y] ** delta[z]) * (pklist[y] ** (h * delta[z]))))
+        dotBCache[z] = dotALoopVal
+        dotCCache[z] = (Slist[z] ** delta[z])
+    dividenconquer(delta, 0, N, incorrectIndices, dotBCache, dotCCache, P, g)
+    output = incorrectIndices
     return output
 
 def SmallExp(bits=80):
@@ -109,9 +166,15 @@ def main():
 
     (P, g, alpha) = setup()
     (pk, sk) = keygen(alpha, "alice")
-    L = ["alice", "bob", "charlie"]
-    (u, S) = sign("alice", pk, sk, L, "message")
-    print(verify(P, g, L, "message", u, S))
+    L = ["alice", "bob"]
+    (Lt, pklist, u1, S1) = sign("alice", pk, sk, L, "message")
+    (Lt, pklist, u2, S2) = sign("alice", pk, sk, L, "message2")
+    Mlist = ["message", "message2"]
+    Slist = [S1, S2]
+    ulist = [u1, u2]
+    incorrectIndices = []
+    batchverify(Lt, Mlist, P, Slist, g, pklist, ulist, incorrectIndices)
+    print(incorrectIndices)
 
 
 if __name__ == '__main__':
