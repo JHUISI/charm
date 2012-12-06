e0e1c2c470f73ddd0613b927cffd3945d64f2245
J Ayo Akinyele <jakinye3@jhu.edu>
2012-12-06 09:38:56 -0500

    tweak

--- auto_batch/timing/avg.py
+++ auto_batch/timing/avg.py
@@ -1,5 +1,21 @@
 import re, sys, os
 
+"""
+Calculate mean and standard deviation of data x[]:
+    mean = {\sum_i x_i \over n}
+    std = sqrt(\sum_i (x_i - mean)^2 \over n-1)
+"""
+def meanstdv(x):
+    from math import sqrt
+    n, mean, std = len(x), 0, 0
+    for a in x:
+        mean = mean + a
+    mean = mean / float(n)
+    for a in x:
+        std = std + (a - mean)**2
+    std = sqrt(std / float(n-1))
+    return mean, std
+
 
 def main(files):
     for f in files:
@@ -9,9 +25,10 @@ def main(files):
         sum = 0
         length = len(i)
         count = 0
+        finalList = []
         for j in i:
-            if re.match("^\d+?\.\d+?$", j): sum += float(j); count += 1    
-        print("file =", f, ": avg =", float(sum / count), ": count =", count)
+            if re.match("^\d+?\.\d+?$", j): sum += float(j); count += 1; finalList.append(float(j))    
+        print("file =", f, ": avg =", float(sum / count), ": count =", count, "stddev = ", meanstdv(finalList))
         file.close()
             
 
