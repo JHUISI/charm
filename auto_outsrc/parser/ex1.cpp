3fbbfca4987268602fbc36b4b7603a0008a18be1
J Ayo Akinyele <jakinye3@jhu.edu>
2012-12-19 15:25:44 -0500

    updates

--- auto_outsrc/parser/ex1.cpp
+++ auto_outsrc/parser/ex1.cpp
@@ -25,7 +25,7 @@ void setup(G1 & g1, G2 & g2, GT & egga, CharmList & msk)
     return;
 }
 
-void keygen(G2 & g2, GT & egga, CharmList & msk, GT & pkBlinded, ZR & blindingFactor0Blinded, CharmList & skBlinded)
+void keygen(G2 & g2, GT & egga, CharmList & msk, GT & pk, GT & pkBlinded, ZR & blindingFactor0Blinded, CharmList & skBlinded)
 {
     ZR zz = group.init(ZR_t);
     ZR a;
@@ -34,7 +34,7 @@ void keygen(G2 & g2, GT & egga, CharmList & msk, GT & pkBlinded, ZR & blindingFa
     G2 d = group.init(G2_t);
     G2 dBlinded = group.init(G2_t);
     GT eggat = group.init(GT_t);
-    GT pk = group.init(GT_t);
+    pk = group.init(GT_t);
     ZR dummyVar2 = group.init(ZR_t);
     ZR dummyVar2Blinded = group.init(ZR_t);
     CharmList sk;
@@ -75,33 +75,61 @@ void encrypt(G1 & g1, GT & pk, GT & M, GT & c0, G1 & c1, G1 & c2, G1 & c3)
     return;
 }
 
-void transform(GT & pk, CharmList & sk, GT & c0, G1 & c1, G1 & c2, G1 & c3, CharmList & transformOutputList)
+void transform(GT & pk, CharmList & skBlinded, GT & c0, G1 & c1, G1 & c2, G1 & c3, CharmList & transformOutputList)
 {
     G2 d;
     ZR dummyVar2;
     GT result = group.init(GT_t);
     
-    d = sk[0].getG2();
-    dummyVar2 = sk[1].getZR();
-    transformOutputList[0] = group.pair(group.mul(c1, group.mul(c2, c3)), d);
-    result = transformOutputList[0];
+    d = skBlinded[0].getG2();
+    //dummyVar2 = skBlinded[1].getZR();
+    transformOutputList.insert(0, group.pair(group.mul(c1, group.mul(c2, c3)), d));
+    result = transformOutputList[0].getGT();
+    cout << "transformList : " << transformOutputList << endl;
     return;
 }
 
-void decout(GT & pk, CharmList & sk, GT & c0, G1 & c1, G1 & c2, G1 & c3, CharmList & transformOutputList, ZR & blindingFactor0Blinded, GT & M)
+void decout(GT & pk, GT & c0, G1 & c1, G1 & c2, G1 & c3, CharmList & transformOutputList, ZR & blindingFactor0Blinded, GT & M)
 {
     G2 d;
     ZR dummyVar2;
     GT result = group.init(GT_t);
     
-    d = sk[0].getG2();
-    dummyVar2 = sk[1].getZR();
-    result = group.exp(transformOutputList[0], blindingFactor0Blinded);
+   // d = sk[0].getG2();
+   // dummyVar2 = sk[1].getZR();
+    result = group.exp(transformOutputList[0].getGT(), blindingFactor0Blinded);
     M = group.mul(c0, group.exp(result, -1));
     return;
 }
 
 int main()
 {
+    ZR blindingFactor0Blinded;
+    G1 g1;
+    G2 g2; 
+    GT egga, pk, pkBlinded;
+    CharmList sk, msk, skBlinded, transformOutputList;
+
+    setup(g1, g2, egga, msk);
+
+    keygen(g2, egga, msk, pk, pkBlinded, blindingFactor0Blinded, skBlinded);
+
+    GT M = group.random(GT_t);
+    cout << "M1 :=> " << M.g << endl;
+    GT MII = group.init(GT_t);
+    G1 c1, c2, c3;
+    GT c0 = group.init(GT_t);
+    encrypt(g1, pk, M, c0, c1, c2, c3);
+
+    cout << "c0 : " << c0.g << endl;
+    cout << "c1 : " << c1.g << endl;
+    cout << "c2 : " << c2.g << endl;
+    cout << "c3 : " << c3.g << endl;
+    transform(pk, skBlinded, c0, c1, c2, c3, transformOutputList);
+
+    decout(pk, c0, c1, c2, c3, transformOutputList, blindingFactor0Blinded, MII);
+
+    cout << "M2 :=> " << MII.g << endl;
+
     return 0;
 }
