e3c901d1c8876c867d4556f1415d2e93c29e21ac
J Ayo Akinyele <jakinye3@jhu.edu>
2013-02-05 00:28:48 -0500

    updates

--- auto_outsrc/parser/bfSolver.py
+++ auto_outsrc/parser/bfSolver.py
@@ -68,23 +68,30 @@ s.add( And(nonNilList) )
 
 alpha, t1, t2, t3, t4, r1, r2 = varList
 
-p1, p2, p3, p4, p5 = Bools('p1 p2 p3 p4 p5')
+p1, p2, p3, p4, p5, p6, p7 = Bools('p1 p2 p3 p4 p5 p6 p7')
 
 # rnd can be reused, but reused consistently
 # 1
 s.assert_and_track( And(r2 == nil, t3 != nil), p1 )
 # 2
 s.assert_and_track( And(r2 == nil, t4 != nil), p2 )
-# 3
-s.assert_and_track( And(alpha != t1, r2 == nil, t1 != nil), p3 )
-# 4
-# s.add( And(alpha != t2), Or(r1 == nil, t2 != nil)) )
-# mul rule
-s.assert_and_track( Or(And(alpha == nil, t2 != nil), And(alpha != nil, t2 == nil)), p4 )
+# 3 : alpha * t1 + r2 * t1
+# need a way to translate the expression to the following:
+#s.assert_and_track( Or(And(alpha == nil, t1 != nil), And(alpha != nil, t1 == nil)), p3 )
+#s.assert_and_track( Or(And(r2 == nil, t1 != nil), And(r2 != nil, t1 == nil)), p3 )
+#s.assert_and_track( And(Or(alpha == r2, alpha == t1), Or(t1 == r2, t1 == t1)), p3 )
+
+# 4 : alpha * t2 + r1 * t2
+#s.assert_and_track( Or(And(alpha == nil, t2 != nil), And(alpha != nil, t2 == nil)), p4 ) 
+#s.assert_and_track( Or(And(r1 == nil, t2 != nil), And(r1 != nil, t2 == nil)), p4 )
+#s.assert_and_track( And(Or(alpha == r1, alpha == t2), Or(t2 == r1, t2 == t2)), p4 )
 
 # add rule
-# 5
-## s.add( And(r1 == nil, t1
+# 5 : r1 * t1 * t2 + r2 * t3 * t4 => {'MUL0': [ 'r1', 't1', 't2' ],  'MUL1': [ 'r2', 't3', 't4' ], 'ADD0': ['MUL0', 'MUL1'], 'root':'ADD0' }
+s.assert_and_track( Or(And(r1 == nil, t1 == nil, t2 != nil), And(r1 == nil, t1 != nil, t2 != nil), And(r1 != nil, t1 == nil, t2 == nil)), p5 )
+s.assert_and_track( Or(And(r2 == nil, t3 == nil, t4 != nil), And(r2 == nil, t3 != nil, t4 != nil), And(r2 != nil, t3 == nil, t4 == nil)), p6 )
+s.assert_and_track( And(Or(r1 == r2, r1 == t3, r1 == t4), Or(t1 == r2, t1 == t3, t1 == t4), Or(t2 == r2, t2 == t3, t2 == t4)), p7 )
+
 print(s)
 print(s.check())
 if s.check() != unsat:
