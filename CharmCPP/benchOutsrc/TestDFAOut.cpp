cb9e0e4e54060e6a765e3a41a100c0f124df7d27
J Ayo Akinyele <jakinye3@jhu.edu>
2013-02-20 01:03:47 -0500

    really adding DFA

---
+++ CharmCPP/benchOutsrc/TestDFAOut.cpp
@@ -0,0 +1,294 @@
+#include "TestDFAOut.h"
+
+void Dfa12::setup(CharmListStr & alphabet, CharmList & mpk, G1 & msk)
+{
+    G1 g;
+    G1 z;
+    G1 hstart;
+    G1 hend;
+    int A = 0;
+    string a;
+    CharmListG1 h;
+    ZR alpha;
+    GT egg = group.init(GT_t);
+    g = group.random(G1_t);
+    z = group.random(G1_t);
+    hstart = group.random(G1_t);
+    hend = group.random(G1_t);
+    A = alphabet.length();
+    for (int i = 0; i < A; i++)
+    {
+        a = dfaUtil.getString(alphabet[i]);
+        h.insert(a, group.random(G1_t));
+    }
+    alpha = group.random(ZR_t);
+    egg = group.exp(group.pair(g, g), alpha);
+    msk = group.exp(g, group.neg(alpha));
+    mpk.insert(0, egg);
+    mpk.insert(1, g);
+    mpk.insert(2, z);
+    mpk.insert(3, h);
+    mpk.insert(4, hstart);
+    mpk.insert(5, hend);
+    return;
+}
+
+void Dfa12::keygen(CharmList & mpk, G1 & msk, CharmListInt & Q, CharmMetaListInt & T, CharmListInt & F, ZR & bf0, CharmList & skBlinded)
+{
+    GT egg;
+    G1 g;
+    G1 z;
+    CharmListG1 h;
+    G1 hstart;
+    G1 hend;
+    int qlen = 0;
+    CharmListG1 D;
+    ZR rstart;
+    G1 Kstart1;
+    G1 Kstart1Blinded;
+    G1 Kstart2;
+    G1 Kstart2Blinded;
+    int Tlen = 0;
+    ZR r;
+    CharmListInt t;
+    int t0 = 0;
+    int t1 = 0;
+    string t2;
+    string key;
+    CharmListG1 K1;
+    CharmListG1 K2;
+    CharmListG1 K3;
+    CharmListG1 K1Blinded;
+    CharmListG1 K2Blinded;
+    CharmListG1 K3Blinded;
+    int Flen = 0;
+    int x = 0;
+    ZR rx;
+    CharmListG1 KendList1;
+    CharmListG1 KendList2;
+    CharmListG1 KendList1Blinded;
+    CharmListG1 KendList2Blinded;
+    bf0 = group.random(ZR_t);
+    
+    egg = mpk[0].getGT();
+    g = mpk[1].getG1();
+    z = mpk[2].getG1();
+    h = mpk[3].getListG1();
+    hstart = mpk[4].getG1();
+    hend = mpk[5].getG1();
+    qlen = Q.length();
+    for (int i = 0; i < qlen+1; i++)
+    {
+        D.insert(i, group.random(G1_t));
+    }
+    rstart = group.random(ZR_t);
+    Kstart1 = group.mul(D[0], group.exp(hstart, rstart));
+    Kstart1Blinded = group.exp(Kstart1, group.div(1, bf0));
+    Kstart2 = group.exp(g, rstart);
+    Kstart2Blinded = group.exp(Kstart2, group.div(1, bf0));
+    Tlen = T.length();
+    for (int i = 0; i < Tlen; i++)
+    {
+        r = group.random(ZR_t);
+        t = T[i];
+        t0 = t[0];
+        t1 = t[1];
+        t2 = dfaUtil.getString(t[2]);
+        key = dfaUtil.hashToKey(t);
+        K1.insert(key, group.mul(group.exp(D[t0], -1), group.exp(z, r)));
+        K2.insert(key, group.exp(g, r));
+        K3.insert(key, group.mul(D[t1], group.exp(h[t2], r)));
+    }
+    CharmListStr K1_keys = K1.strkeys();
+    int K1_len = K1_keys.length();
+    for (int y_var = 0; y_var < K1_len; y_var++)
+    {
+        string y = K1_keys[y_var];
+        K1Blinded.insert(y, group.exp(K1[y], group.div(1, bf0)));
+    }
+    CharmListStr K2_keys = K2.strkeys();
+    int K2_len = K2_keys.length();
+    for (int y_var = 0; y_var < K2_len; y_var++)
+    {
+        string y = K2_keys[y_var];
+        K2Blinded.insert(y, group.exp(K2[y], group.div(1, bf0)));
+    }
+    CharmListStr K3_keys = K3.strkeys();
+    int K3_len = K3_keys.length();
+    for (int y_var = 0; y_var < K3_len; y_var++)
+    {
+        string y = K3_keys[y_var];
+        K3Blinded.insert(y, group.exp(K3[y], group.div(1, bf0)));
+    }
+    Flen = F.length();
+    for (int i = 0; i < Flen; i++)
+    {
+        x = F[i];
+        rx = group.random(ZR_t);
+        KendList1.insert(x, group.mul(msk, group.mul(D[x], group.exp(hend, rx))));
+        KendList2.insert(x, group.exp(g, rx));
+    }
+    CharmListInt KendList1_keys = KendList1.keys();
+    int KendList1_len = KendList1_keys.length();
+    for (int y_var = 0; y_var < KendList1_len; y_var++)
+    {
+        int y = KendList1_keys[y_var];
+        KendList1Blinded.insert(y, group.exp(KendList1[y], group.div(1, bf0)));
+    }
+    CharmListInt KendList2_keys = KendList2.keys();
+    int KendList2_len = KendList2_keys.length();
+    for (int y_var = 0; y_var < KendList2_len; y_var++)
+    {
+        int y = KendList2_keys[y_var];
+        KendList2Blinded.insert(y, group.exp(KendList2[y], group.div(1, bf0)));
+    }
+    skBlinded.insert(0, Kstart1Blinded);
+    skBlinded.insert(1, Kstart2Blinded);
+    skBlinded.insert(2, KendList1Blinded);
+    skBlinded.insert(3, KendList2Blinded);
+    skBlinded.insert(4, K1Blinded);
+    skBlinded.insert(5, K2Blinded);
+    skBlinded.insert(6, K3Blinded);
+    return;
+}
+
+void Dfa12::encrypt(CharmList & mpk, CharmListStr & w, GT & M, CharmList & ct)
+{
+    GT egg;
+    G1 g;
+    G1 z;
+    CharmListG1 h;
+    G1 hstart;
+    G1 hend;
+    int l = 0;
+    CharmListZR s;
+    GT Cm = group.init(GT_t);
+    CharmListG1 C1;
+    CharmListG1 C2;
+    string a;
+    G1 Cend1;
+    G1 Cend2;
+    
+    egg = mpk[0].getGT();
+    g = mpk[1].getG1();
+    z = mpk[2].getG1();
+    h = mpk[3].getListG1();
+    hstart = mpk[4].getG1();
+    hend = mpk[5].getG1();
+    l = w.length();
+    for (int i = 0; i < l+1; i++)
+    {
+        s.insert(i, group.random(ZR_t));
+    }
+    Cm = group.mul(M, group.exp(egg, s[l]));
+    C1.insert(0, group.exp(g, s[0]));
+    C2.insert(0, group.exp(hstart, s[0]));
+    for (int i = 1; i < l+1; i++)
+    {
+        a = dfaUtil.getString(w[i]);
+        C1.insert(i, group.exp(g, s[i]));
+        C2.insert(i, group.mul(group.exp(h[a], s[i]), group.exp(z, s[i-1]))); // fix h inf here
+    }
+    Cend1 = group.exp(g, s[l]);
+    Cend2 = group.exp(hend, s[l]);
+    ct.insert(0, Cend1);
+    ct.insert(1, Cend2);
+    ct.insert(2, w);
+    ct.insert(3, C1);
+    ct.insert(4, C2);
+    ct.insert(5, Cm);
+    return;
+}
+
+void Dfa12::transform(CharmList & skBlinded, CharmList & ct, CharmList & transformOutputList, int & l, CharmMetaListInt & Ti, int & x, CharmList & transformOutputListForLoop)
+{
+    G1 Kstart1Blinded;
+    G1 Kstart2Blinded;
+    CharmListG1 KendList1Blinded;
+    CharmListG1 KendList2Blinded;
+    CharmListG1 K1Blinded;
+    CharmListG1 K2Blinded;
+    CharmListG1 K3Blinded;
+    G1 Cend1;
+    G1 Cend2;
+    CharmListStr w; // type gets lost
+    CharmListG1 C1;
+    CharmListG1 C2;
+    GT Cm;
+    CharmListGT B;
+    string key;
+    int j = 0;
+    GT result0 = group.init(GT_t);
+//    int x = 0;
+    GT result1 = group.init(GT_t);
+    
+    Kstart1Blinded = skBlinded[0].getG1();
+    Kstart2Blinded = skBlinded[1].getG1();
+    KendList1Blinded = skBlinded[2].getListG1();
+    KendList2Blinded = skBlinded[3].getListG1();
+    K1Blinded = skBlinded[4].getListG1();
+    K2Blinded = skBlinded[5].getListG1();
+    K3Blinded = skBlinded[6].getListG1();
+    
+    Cend1 = ct[0].getG1();
+    Cend2 = ct[1].getG1();
+    w = ct[2].getListStr();
+    C1 = ct[3].getListG1();
+    C2 = ct[4].getListG1();
+    Cm = ct[5].getGT();
+    transformOutputList.insert(3, Cm);
+    transformOutputList.insert(2, w);
+    l = w.length();
+//    if ( ( (dfaUtil.accept(w)) == (false) ) )
+//    {
+//
+//        return;
+//    }
+//    Ti = dfaUtil.getTransitions(w);
+    transformOutputList.insert(0, group.mul(group.pair(C1[0], Kstart1Blinded), group.pair(group.exp(C2[0], -1), Kstart2Blinded)));
+    B.insert(0, transformOutputList[0].getGT());
+    for (int i = 1; i < l+1; i++)
+    {
+
+        key = dfaUtil.hashToKey(Ti[i]);
+        j = (i - 1);
+        transformOutputListForLoop.insert(10+7*i, group.mul(group.mul(group.pair(C1[j], K1Blinded[key]), group.pair(group.exp(C2[i], -1), K2Blinded[key])), group.pair(C1[i], K3Blinded[key])));
+        result0 = transformOutputListForLoop[10+7*i].getGT();
+    }
+    //x = dfaUtil.getAcceptState(Ti);
+    transformOutputList.insert(1, group.mul(group.pair(group.exp(Cend1, -1), KendList1Blinded[x]), group.pair(Cend2, KendList2Blinded[x])));
+    // result1 = transformOutputList[1].getGT();
+    return;
+}
+
+void Dfa12::decout(CharmList & transformOutputList, ZR & bf0, int l, CharmMetaListInt & Ti, CharmList & transformOutputListForLoop, GT & M)
+{
+    GT Cm = group.init(GT_t);
+    CharmListStr w; // type gets lost
+    CharmListGT B;
+    string key;
+    int j = 0;
+    GT result0 = group.init(GT_t);
+    GT result1 = group.init(GT_t);
+    GT Bend = group.init(GT_t);
+    Cm = transformOutputList[3].getGT();
+    w = transformOutputList[2].getListStr();
+//    if ( ( (dfaUtil.accept(w)) == (false) ) )
+//    {
+//    	return;
+//    }
+    B.insert(0, group.exp(transformOutputList[0].getGT(), bf0));
+    for (int i = 1; i < l+1; i++)
+    {
+
+        key = dfaUtil.hashToKey(Ti[i]);
+        j = (i - 1);
+        result0 = group.exp(transformOutputListForLoop[10+7*i].getGT(), bf0);
+        B.insert(i, group.mul(B[i-1], result0));
+    }
+    result1 = group.exp(transformOutputList[1].getGT(), bf0);
+    Bend = group.mul(B[l], result1);
+    M = group.mul(Cm, group.exp(Bend, -1));
+    return;
+}
+
---
+++ CharmCPP/benchOutsrc/benchDFAOut.cpp
@@ -0,0 +1,168 @@
+
+#include "TestDFAOut.h"
+
+string getRandomHexString(int len)
+{
+	string hex = "0x";
+	if(len == 2) return hex;
+	else if(len > 2) {
+		string alpha = "ABCDEFabcdef0123456789";
+		int val, alpha_len = alpha.size();
+		for(int i = 0; i < len-2; i++)
+		{
+			val = (int) (rand() % alpha_len);
+			hex +=  alpha[val];
+		}
+		cout << "Hex Value: '" << hex << "'" << endl;
+	}
+	else {
+		cout << "getRandomHexString: invalid len => " << len << endl;
+		return "";
+	}
+	return hex;
+
+}
+
+void benchmarkDFA(Dfa12 & dfa12, ofstream & outfile1, ofstream & outfile2, int wStringCount, int iterationCount, int increment, CharmListStr & transformResults, CharmListStr & decoutResults)
+{
+	Benchmark benchT, benchD;
+	string letters = "xABCDEFabcdef0123456789";
+	dfa12.dfaUtil.constructDFA("0x(0|1|2|3|4|5|6|7|8|9|a|b|c|d|e|f|A|B|C|D|E|F)*", letters);
+	CharmList mpk, skBlinded, ct, transformOutputList, transformOutputListForLoop;
+	CharmListStr alphabet = dfa12.dfaUtil.getAlphabet();
+	CharmListStr w;
+	CharmListInt Q = dfa12.dfaUtil.getStates(), F = dfa12.dfaUtil.getAcceptStates(); // get all states, and all accept states
+	CharmMetaListInt T = dfa12.dfaUtil.getTransitions(); // get all transitions in DFA
+	ZR bf0;
+	G1 msk;
+	GT M, newM, Cm;
+
+	//cout << "Q:\n" << Q << endl;
+	//cout << "F:\n" << F << endl;
+	//cout << "T:\n" << T << endl;
+
+	dfa12.setup(alphabet, mpk, msk);
+
+	//cout << "MPK:\n" << mpk << endl;
+	//cout << "MSK:\n" << convert_str(msk) << endl;
+	dfa12.keygen(mpk, msk, Q, T, F, bf0, skBlinded);
+
+	//cout << "SK:\n" << skBlinded << endl;
+
+	w = dfa12.dfaUtil.getSymbols(getRandomHexString(wStringCount)); // "aba"
+	M = dfa12.group.random(GT_t);
+	dfa12.encrypt(mpk, w, M, ct);
+	double tf_in_ms, de_in_ms;
+
+	if(dfa12.dfaUtil.accept(w)) {
+		cout << "isAccept: true" << endl;
+		CharmMetaListInt Ti = dfa12.dfaUtil.getTransitions(w); // 1
+		cout << "transitions to decrypt: " << Ti.length() << endl;
+		int x = dfa12.dfaUtil.getAcceptState(Ti); // 2
+		int l = 0;
+		stringstream s1, s2;
+		for(int i = 0; i < iterationCount; i += increment) {
+			benchT.start();
+			dfa12.transform(skBlinded, ct, transformOutputList, l, Ti, x, transformOutputListForLoop);
+			benchT.stop();
+			tf_in_ms = benchT.computeTimeInMilliseconds();
+
+			benchD.start();
+			dfa12.decout(transformOutputList, bf0, l, Ti, transformOutputListForLoop, newM);
+			benchD.stop();
+			de_in_ms = benchD.computeTimeInMilliseconds();
+		}
+		cout << "Transform avg: " << benchT.getAverage() << " ms" << endl;
+		s1 << wStringCount << " " << benchT.getAverage() << endl;
+		outfile1 << s1.str();
+	    transformResults[wStringCount] = benchT.getRawResultString();
+
+		cout << "Decout avg: " << benchD.getAverage() << " ms" << endl;
+		s2 << wStringCount << " " << benchD.getAverage() << endl;
+		outfile2 << s2.str();
+		decoutResults[wStringCount] = benchD.getRawResultString();
+//		cout << convert_str(M) << endl;
+//		cout << convert_str(newM) << endl;
+		if(M == newM) {
+		  cout << "Successful Decryption!" << endl << endl;
+		}
+		else {
+		  cout << "FAILED Decryption." << endl << endl;
+		}
+	}
+	else {
+		cout << "isAccept: false" << endl;
+	}
+    return;
+}
+
+int main(int argc, const char *argv[])
+{
+	string FIXED = "fixed", RANGE = "range";
+	if(argc != 5) { cout << "Usage " << argv[0] << ": [ iterationCount => 10 ] [ wStringCount => 1000 ] [ incrementCount => 25 ] [ 'fixed' or 'range' ]" << endl; return -1; }
+
+	int iterationCount = atoi( argv[1] );
+	int wStringCount = atoi( argv[2] );
+	int incrementCount = atoi( argv[3] );
+	string fixOrRange = string( argv[4] );
+	cout << "iterationCount: " << iterationCount << endl;
+	cout << "wStringCount: " << wStringCount << endl;
+	cout << "incrementCount: " << incrementCount << endl;
+	cout << "measurement: " << fixOrRange << endl;
+
+	Dfa12 dfa12;
+	string filename = string(argv[0]);
+	stringstream s3, s4;
+	ofstream outfile1, outfile2, outfile3, outfile4;
+	string f1 = filename + "_transform.dat";
+	string f2 = filename + "_decout.dat";
+	string f3 = filename + "_transform_raw.txt";
+	string f4 = filename + "_decout_raw.txt";
+	outfile1.open(f1.c_str());
+	outfile2.open(f2.c_str());
+	outfile3.open(f3.c_str());
+	outfile4.open(f4.c_str());
+
+	CharmListStr transformResults, decoutResults;
+	if(isEqual(fixOrRange, RANGE)) {
+		for(int i = 2; i <= wStringCount; i++) {
+			cout << "Benchmark with " << i << " wStringCount." << endl;
+			benchmarkDFA(dfa12, outfile1, outfile2, i, iterationCount, incrementCount, transformResults, decoutResults);
+		}
+		s3 << transformResults << endl;
+		s4 << decoutResults << endl;
+	}
+	else if(isEqual(fixOrRange, FIXED)) {
+		cout << "Benchmark with " << wStringCount << " wStringCount." << endl;
+		benchmarkDFA(dfa12, outfile1, outfile2, wStringCount, iterationCount, incrementCount, transformResults, decoutResults);
+		s3 << wStringCount << " " << transformResults[wStringCount] << endl;
+		s4 << wStringCount << " " << decoutResults[wStringCount] << endl;
+	}
+	else {
+		cout << "invalid option." << endl;
+		return -1;
+	}
+
+	outfile3 << s3.str();
+	outfile4 << s4.str();
+	outfile1.close();
+	outfile2.close();
+	outfile3.close();
+	outfile4.close();
+//	cout << "<=== Transform benchmarkWATERS breakdown ===>" << endl;
+//	cout << transformResults << endl;
+//	cout << "<=== Transform benchmarkWATERS breakdown ===>" << endl;
+//	cout << "<=== Decout benchmarkWATERS breakdown ===>" << endl;
+//	cout << decoutResults << endl;
+//	cout << "<=== Decout benchmarkWATERS breakdown ===>" << endl;
+
+	return 0;
+}
+
+//	Dfa12 dfa12;
+//	dfa12.dfaUtil.constructDFA("ab*a", "ab");
+//	CharmListStr w = dfa12.dfaUtil.getSymbols("aba");
+//	CharmMetaListInt Ti = dfa12.dfaUtil.getTransitions(w); // 1
+//	int x = dfa12.dfaUtil.getAcceptState(Ti); // 2
+
+//	dfa12.dfaUtil.constructDFA("(0|1|2|3|4|5|6|7|8|9)*-(a|b|c)*-(0|1|2|3|4|5|6|7|8|9)*", "abc0123456789-"); // "abcdefghijklmnopqrstuvwxyz0123456789"
