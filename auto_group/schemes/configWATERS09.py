a0386acfaa8ab0638f49eb2d7403cc79596e4e34
J Ayo Akinyele <jakinye3@jhu.edu>
2013-03-18 18:12:42 -0400

    more updates

---
+++ auto_group/schemes/configWATERS09.py
@@ -0,0 +1,13 @@
+schemeType = "PKENC"
+
+setupFuncName = "setup"
+keygenFuncName = "keygen"
+encryptFuncName = "encrypt"
+decryptFuncName = "decrypt"
+
+masterPubVars = ["pk"]
+masterSecVars = ["msk"]
+
+keygenPubVar = ["pk"]
+keygenSecVar = "sk"
+ciphertextVar = "ct"
