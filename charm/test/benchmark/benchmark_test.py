142e1275e905b2bf01a837cf8422d967e55f83dd
Brandon Sloane <btsloane@verizon.net>
2013-07-03 15:06:33 -0400

    Added unittest for Benchmark and Serialization APIs

---
+++ charm/test/benchmark/benchmark_test.py
@@ -0,0 +1,139 @@
+from charm.toolbox.pairinggroup import PairingGroup,ZR,G1,G2,GT,pair
+from charm.toolbox.ecgroup import ECGroup,ZR,G
+from charm.toolbox.eccurve import prime192v1
+from charm.core.math.elliptic_curve import *
+from charm.core.math.integer import *
+
+import unittest
+
+debug = False
+
+def isSaneBenchmark(dct):
+    isSane=True
+    for val in dct.values():
+        if(type(val)==list):
+            for v in val:
+                isSane&=v>=0
+        else:
+            isSane&=val>=0
+    return isSane
+
+class BenchmarkTest(unittest.TestCase):
+
+    def testPairing(self):    
+        trials = 10
+        trials2 = trials * 3 
+        group = PairingGroup("SS512")
+        g = group.random(G1)
+        h = group.random(G1)
+        i = group.random(G2)
+
+        self.assertTrue(group.InitBenchmark())
+        group.StartBenchmark(["RealTime", "Exp", "Pair"])
+        for a in range(trials):
+            j = g * h 
+            k = i ** group.random(ZR)
+            t = (j ** group.random(ZR)) / h 
+            n = pair(h, i)
+        group.EndBenchmark()
+       
+        msmtDict = group.GetGeneralBenchmarks()
+        self.assertTrue(isSaneBenchmark(msmtDict))        
+
+        self.assertTrue(group.InitBenchmark())
+        
+        group.StartBenchmark(["CpuTime", "Mul", "Pair"])
+        for a in range(trials2):
+            j = g * h 
+            k = i ** group.random(ZR)
+            n = pair(h, i)
+        group.EndBenchmark()
+        
+        msmtDict = group.GetGeneralBenchmarks()
+        self.assertTrue(isSaneBenchmark(msmtDict))        
+
+    def testECGroup(self):
+        trials = 10
+        group = ECGroup(prime192v1)
+        g = group.random(G)
+        h = group.random(G)
+        i = group.random(G)
+        
+        self.assertTrue(group.InitBenchmark())
+        group.StartBenchmark([RealTime, Mul, Div, Exp, Granular])
+        for a in range(trials):
+            j = g * h 
+            k = h ** group.random(ZR)
+            t = (j ** group.random(ZR)) / k 
+        group.EndBenchmark()
+        
+        msmtDict = group.GetGeneralBenchmarks()
+        self.assertTrue(isSaneBenchmark(msmtDict))        
+        
+        granDict = group.GetGranularBenchmarks()
+        self.assertTrue(isSaneBenchmark(granDict))        
+        
+        self.assertTrue(group.InitBenchmark())
+        group.StartBenchmark([RealTime, Mul, Div, Exp, Granular])
+        for a in range(trials*2):
+            j = g * h 
+            k = h ** group.random(ZR)
+            t = (j ** group.random(ZR)) / k 
+        group.EndBenchmark()
+        
+        msmtDict = group.GetGeneralBenchmarks()
+        granDict = group.GetGranularBenchmarks()
+        self.assertTrue(isSaneBenchmark(msmtDict))        
+        self.assertTrue(isSaneBenchmark(granDict))        
+    
+    def testInteger(self):
+        count = 15
+        time_in_ms = 1000
+        
+        a = integer(10)
+        
+        self.assertTrue(InitBenchmark())
+        StartBenchmark([RealTime, Exp, Mul])
+        for k in range(count):
+            r = randomPrime(512)
+            s = r * (r ** a)
+            j = r * (r ** a)
+        EndBenchmark()
+        msmtDict = GetGeneralBenchmarks()
+        self.assertTrue(isSaneBenchmark(msmtDict))        
+        
+        self.assertTrue(InitBenchmark())
+        StartBenchmark([RealTime, Exp, Mul, Add, Sub])
+        for k in range(count):
+            r = randomPrime(512)
+            s = r * (r ** a)
+            j = r * (r ** a)
+            u = s + j - j 
+        EndBenchmark()
+        msmtDict = GetGeneralBenchmarks()
+        self.assertTrue(isSaneBenchmark(msmtDict))        
+
+    def testInterleave(self):
+        trials = 10
+        trials2 = trials * 3 
+        group1 = PairingGroup("SS512")
+        group2 = PairingGroup("SS512")
+        
+        g = group1.random(G1)
+        h = group1.random(G1)
+        i = group1.random(G2)
+        
+        self.assertTrue(group1.InitBenchmark())
+        self.assertTrue(group2.InitBenchmark())
+        group1.StartBenchmark(["RealTime", "Exp", "Pair", "Div", "Mul"])
+        for a in range(trials):
+            j = g * h 
+            k = i ** group1.random(ZR)
+            t = (j ** group1.random(ZR)) / h 
+            n = pair(h, i)
+        group1.EndBenchmark()
+        msmtDict = group1.GetGeneralBenchmarks()
+        self.assertTrue(isSaneBenchmark(msmtDict))        
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ charm/test/serialize/serialize_test.py
@@ -0,0 +1,39 @@
+from charm.core.engine.util import objectToBytes,bytesToObject
+from charm.toolbox.integergroup import IntegerGroup, integer
+from charm.toolbox.pairinggroup import PairingGroup
+from charm.toolbox.ecgroup import ECGroup
+from charm.toolbox.eccurve import prime192v1
+import unittest
+
+debug = False
+
+class SerializeTest(unittest.TestCase):
+    def testIntegerGroup(self):    
+        groupObj = IntegerGroup()
+        p = integer(148829018183496626261556856344710600327516732500226144177322012998064772051982752493460332138204351040296264880017943408846937646702376203733370973197019636813306480144595809796154634625021213611577190781215296823124523899584781302512549499802030946698512327294159881907114777803654670044046376468983244647367)
+        data={'p':p,'String':"foo",'list':[p,{}]}
+
+        x=objectToBytes(data,groupObj)
+        data2=bytesToObject(x,groupObj)
+        self.assertEqual(data,data2)
+    
+    def testPairingGroup(self):    
+        groupobj = PairingGroup('SS512')
+        p=groupobj.random()
+        data={'p':p,'String':"foo",'list':[p,{}]}
+
+        x=objectToBytes(data,groupobj)
+        data2=bytesToObject(x,groupobj)
+        self.assertEqual(data,data2)
+        
+    def testECGroup(self):    
+        groupObj = ECGroup(prime192v1)
+        p=groupObj.random()
+        data={'p':p,'String':"foo",'list':[p,{}]}
+
+        x=objectToBytes(data,groupObj)
+        data2=bytesToObject(x,groupObj)
+        self.assertEqual(data,data2)
+    
+if __name__ == "__main__":
+    unittest.main()
