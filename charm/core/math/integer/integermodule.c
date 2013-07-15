2a86a4fc454d80c82ef3e2521947709f50b32804
Brandon Sloane <btsloane@verizon.net>
2013-07-15 10:05:45 -0400

    Bugfixes

--- charm/core/math/integer/integermodule.c
+++ charm/core/math/integer/integermodule.c
@@ -1953,7 +1953,7 @@ static PyObject *deserialize(PyObject *self, PyObject *args) {
 	uint8_t *serial_buf2 = (uint8_t *) PyBytes_AsString(bytesObj);
 	int serial_buf2_len = strlen((char *) serial_buf2);
 	uint8_t serial_buf[serial_buf2_len + 1];
-	memset(serial_buf, 0, serial_buf2_len);
+	memset(serial_buf, 0, serial_buf2_len + 1);
 	memcpy(serial_buf, serial_buf2, serial_buf2_len);
 	/* get integer value */
 	char delim[] = ":";
@@ -2437,7 +2437,7 @@ PyInit_integer(void) {
 #define INITERROR return
 void initinteger(void) {
 #endif
-	PyObject *m;
+	PyObject *m=NULL;
 	if (PyType_Ready(&IntegerType) < 0)
 		CLEAN_EXIT;
 #ifdef BENCHMARK_ENABLED
@@ -2487,7 +2487,9 @@ LEAVE:
 	if (PyErr_Occurred()) {
 		printf("ERROR: module load failed!\n");
 		PyErr_Clear();
-		Py_XDECREF(m);
+        if(m!=NULL){
+		    Py_XDECREF(m);
+        }
 		INITERROR;
    }
 
