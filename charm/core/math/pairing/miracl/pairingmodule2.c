211600ea7bd6c492463bb2ca93d4d505661663c8
J Ayo Akinyele <jakinye3@jhu.edu>
2013-07-22 10:04:48 -0400

    minor fixes

--- charm/core/math/pairing/miracl/pairingmodule2.c
+++ charm/core/math/pairing/miracl/pairingmodule2.c
@@ -316,13 +316,13 @@ void	Element_dealloc(Element* self)
 int hash_to_bytes(uint8_t *input_buf, int input_len, uint8_t *output_buf, int hash_len, uint8_t hash_prefix)
 {
 	SHA256_CTX sha2;
-	int i, new_input_len = input_len + 1; // extra byte for prefix
+	int i, new_input_len = input_len + 2; // extra byte for prefix
 	uint8_t first_block = 0;
 	uint8_t new_input[new_input_len+1];
 //	printf("orig input => \n");
 //	printf_buffer_as_hex(input_buf, input_len);
 
-	memset(new_input, 0, new_input_len);
+	memset(new_input, 0, new_input_len+1);
 	new_input[0] = first_block; // block number (always 0 by default)
 	new_input[1] = hash_prefix; // set hash prefix
 	memcpy((uint8_t *)(new_input+2), input_buf, input_len); // copy input bytes
