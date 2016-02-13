94dd9bc02743b31c7778fe21173993a1d93c7b4e
J. Ayo Akinyele <ayo@zeutro.com>
2016-02-13 12:06:51 -0500

    Fix decrypt error when plaintext=0 for Paillier scheme. Closes #97

--- charm/schemes/pkenc/pkenc_paillier99.py
+++ charm/schemes/pkenc/pkenc_paillier99.py
@@ -3,19 +3,17 @@ Pascal Paillier (Public-Key)
  
 | From: "Public-Key Cryptosystems Based on Composite Degree Residuosity Classes" 
 | Published in: EUROCRYPT 1999
-| Available from: http://eprint.iacr.org/2009/309.pdf
+| Available from: http://link.springer.com/chapter/10.1007%2F3-540-48910-X_16
 | Notes: 
 
-* type:           public-key encryption (public key)
-* setting:        Integer
+* type       public-key encryption (public key)
+* setting:   Integer
 
 :Authors:    J Ayo Akinyele
-:Date:            4/2011
+:Date:       4/2011 (updated 2/2016)
 '''
-#from charm.integer import *
-from charm.toolbox.integergroup import lcm,integer
+from charm.toolbox.integergroup import lcm,integer,toInt
 from charm.toolbox.PKEnc import PKEnc
-from charm.core.engine.util import *
 
 debug = False
 """A ciphertext class with homomorphic properties"""
@@ -32,11 +30,7 @@ class Ciphertext(dict):
     >>> msg_1=12345678987654321
     >>> msg_2=12345761234123409
     >>> msg_3 = msg_1 + msg_2
-    
-    >>> msg_1 = pai.encode(public_key['n'], msg_1)
-    >>> msg_2 = pai.encode(public_key['n'], msg_2)
-    >>> msg_3 = pai.encode(public_key['n'], msg_3) 
-    
+        
     >>> cipher_1 = pai.encrypt(public_key, msg_1)
     >>> cipher_2 = pai.encrypt(public_key, msg_2)
     >>> cipher_3 = cipher_1 + cipher_2
@@ -81,13 +75,17 @@ class Pai99(PKEnc):
         group = groupObj
     
     def L(self, u, n):
-        return integer(int(u) - 1) / n
+        # computes L(u) => ((u - 1) / n)
+        U = integer(int(u) - 1)
+        if int(U) == 0:
+            return integer(0, n)
+        return U / n
                 
     def keygen(self, secparam=1024):
         (p, q, n) = group.paramgen(secparam)
-        g = n + 1
         lam = lcm(p - 1, q - 1)
         n2 = n ** 2
+        g = group.random(n2)
         u = (self.L(((g % n2) ** lam), n) % n) ** -1
         pk, sk = {'n':n, 'g':g, 'n2':n2}, {'lamda':lam, 'u':u}
         return (pk, sk)
@@ -100,8 +98,9 @@ class Pai99(PKEnc):
     
     def decrypt(self, pk, sk, ct):
         n, n2 = pk['n'], pk['n2']
-        return ((self.L(ct['c'] ** sk['lamda'], n) % n) * sk['u']) % n
-    
+        m = ((self.L(ct['c'] ** sk['lamda'], n) % n) * sk['u']) % n
+        return toInt(m)
+
     def encode(self, modulus, message):
         # takes a string and represents as a bytes object
         elem = integer(message)
