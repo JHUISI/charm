87ae1bb2b2ee3ead317678334ec3c7bdfbd4d2c5
Dennis Schroer <dev@dennisschroer.nl>
2016-06-29 13:08:52 +0200

    Cleanup and documentation

--- charm/schemes/abenc/abenc_maabe_rw15.py
+++ charm/schemes/abenc/abenc_maabe_rw15.py
@@ -18,6 +18,7 @@ Rouselakis - Waters Efficient Statically-Secure Large-Universe Multi-Authority A
 from charm.toolbox.pairinggroup import *
 from charm.toolbox.secretutil import SecretUtil
 from charm.toolbox.ABEncMultiAuth import ABEncMultiAuth
+import re
 
 debug = False
 
@@ -58,7 +59,7 @@ class MaabeRW15(ABEncMultiAuth):
     >>> user_keys = {'GID': gid, 'keys': merge_dicts(user_keys1, user_keys2)}
 
         Create a random message
-    >>> message = maabe.random_message()
+    >>> message = group.random(GT)
 
         Encrypt the message
     >>> access_policy = '(STUDENT@UT or PROFESSOR@OU) and (STUDENT@UT or MASTERS@OU)'
@@ -70,206 +71,139 @@ class MaabeRW15(ABEncMultiAuth):
     True
     """
 
-    def random_message(self):
-        return group.random(GT)
-
-    def exp(self, value):
-        return group.init(ZR, value)
-
-    def get_authority(self, x):
-        i = x.find("@")
-        if i == -1:
-            print("Error: No @ char in [attribute@authority] name")
-            return
-
-        j = x.find("_")
-        if (j == -1):
-            return x[i + 1:]
-        else:
-            return x[i + 1:j]
-
-    def extract_attribute_name(self, attribute):
-        i = attribute.rfind("_")
-        if i == -1:
-            return attribute
-        else:
-            return attribute[:i]
-
-    def __init__(self, groupObj, verbose=False):
-        super(MaabeRW15, self).__init__()
-        global util, group
-        group = groupObj
-        util = SecretUtil(group, verbose)
+    def __init__(self, group, verbose=False):
+        ABEncMultiAuth.__init__(self)
+        self.group = group
+        self.util = SecretUtil(group, verbose)
 
     def setup(self):
-        g1 = group.random(G1)
-        g2 = group.random(G2)
+        g1 = self.group.random(G1)
+        g2 = self.group.random(G2)
         egg = pair(g1, g2)
-        H = lambda x: group.hash(x, G2)
-        F = lambda x: group.hash(x, G2)
+        H = lambda x: self.group.hash(x, G2)
+        F = lambda x: self.group.hash(x, G2)
         gp = {'g1': g1, 'g2': g2, 'egg': egg, 'H': H, 'F': F}
         return gp
 
+    def unpack_attribute(self, attribute):
+        """
+        Unpacks an attribute in attribute name, authority name and index
+        :param attribute: The attribute to unpack
+        :return: The attribute name, authority name and the attribute index, if present.
+
+        >>> group = PairingGroup('SS512')
+        >>> maabe = MaabeRW15(group)
+        >>> maabe.unpack_attribute('STUDENT@UT')
+        ('STUDENT', 'UT', None)
+        >>> maabe.unpack_attribute('STUDENT@UT_2')
+        ('STUDENT', 'UT', '2')
+        """
+        parts = re.split(r"[@_]", attribute)
+        assert len(parts) > 1, "No @ char in [attribute@authority] name"
+        return parts[0], parts[1], None if len(parts) < 3 else parts[2]
+
     def authsetup(self, gp, name):
-        alpha, y = group.random(), group.random()
+        """
+        Setup an attribute authority.
+        :param gp: The global parameters
+        :param name: The name of the authority
+        :return: The public and private key of the authority
+        """
+        alpha, y = self.group.random(), self.group.random()
         egga = gp['egg'] ** alpha
         gy = gp['g1'] ** y
         pk = {'name': name, 'egga': egga, 'gy': gy}
         sk = {'name': name, 'alpha': alpha, 'y': y}
         return pk, sk
 
-    def keygen(self, gp, sk, gid, attr):
-        assert (
-        sk['name'] == self.get_authority(attr), "Error: Attribute ", attr, " does not belong to authority ", sk['name'])
+    def keygen(self, gp, sk, gid, attribute):
+        """
+        Generate a user secret key for the attribute.
+        :param gp: The global parameters.
+        :param sk: The secret key of the attribute authority.
+        :param gid: The global user identifier.
+        :param attribute: The attribute.
+        :return: The secret key for the attribute for the user with identifier gid.
+        """
+        _, auth, _ = self.unpack_attribute(attribute)
+        assert sk['name'] == auth, "Attribute %s does not belong to authority %s" % (attribute, sk['name'])
 
-        t = group.random()
-        K = gp['g2'] ** sk['alpha'] * gp['H'](gid) ** sk['y'] * gp['F'](attr) ** t
-        # K = gp['g2']**sk['alpha'] * gp['F'](attr)**t
+        t = self.group.random()
+        K = gp['g2'] ** sk['alpha'] * gp['H'](gid) ** sk['y'] * gp['F'](attribute) ** t
         KP = gp['g1'] ** t
 
-        return {'user': gid, 'auth': sk['name'], 'attr': attr, 'K': K, 'KP': KP}
+        return {'K': K, 'KP': KP}
 
     def multiple_attributes_keygen(self, gp, sk, gid, attributes):
+        """
+        Generate a dictionary of secret keys for a user for a list of attributes.
+        :param gp: The global parameters.
+        :param sk: The secret key of the attribute authority.
+        :param gid: The global user identifier.
+        :param attributes: The list of attributes.
+        :return: A dictionary with attribute names as keys, and secret keys for the attributes as values.
+        """
         uk = {}
         for attribute in attributes:
             uk[attribute] = self.keygen(gp, sk, gid, attribute)
         return uk
 
     def encrypt(self, pks, gp, message, policy_str):
-        s = group.random()  # secret to be shared
-        w = group.init(ZR, 0)  # 0 to be shared
+        """
+        Encrypt a message under an access policy
+        :param pks: The public keys of the relevant attribute authorities, as dict from authority name to public key.
+        :param gp: The global parameters.
+        :param message: The message to encrypt.
+        :param policy_str: The access policy to use.
+        :return: The encrypted message.
+        """
+        s = self.group.random()  # secret to be shared
+        w = self.group.init(ZR, 0)  # 0 to be shared
 
-        policy = util.createPolicy(policy_str)
-        a_list = util.getAttributeList(policy)
-        # print("\n\n THE A-LIST IS", a_list,"\n\n")
-        # for i in a_list:
-        #	print(self.getAuth(i))
+        policy = self.util.createPolicy(policy_str)
+        attribute_list = self.util.getAttributeList(policy)
 
-        secretShares = util.calculateSharesDict(s, policy)  # These are correctly set to be exponents in Z_p
-        zeroShares = util.calculateSharesDict(w, policy)
+        secret_shares = self.util.calculateSharesDict(s, policy)  # These are correctly set to be exponents in Z_p
+        zero_shares = self.util.calculateSharesDict(w, policy)
 
         C0 = message * (gp['egg'] ** s)
 
         C1, C2, C3, C4 = {}, {}, {}, {}
-        for i in a_list:
-            auth = self.get_authority(i)
-            attr = self.extract_attribute_name(i)  # take out the possible underscore
-            tx = group.random()
-            C1[i] = gp['egg'] ** secretShares[i] * pks[auth]['egga'] ** tx
+        for i in attribute_list:
+            attr, auth, _ = self.unpack_attribute(i)
+            tx = self.group.random()
+            C1[i] = gp['egg'] ** secret_shares[i] * pks[auth]['egga'] ** tx
             C2[i] = gp['g1'] ** (-tx)
-            C3[i] = pks[auth]['gy'] ** tx * gp['g1'] ** zeroShares[i]
+            C3[i] = pks[auth]['gy'] ** tx * gp['g1'] ** zero_shares[i]
             C4[i] = gp['F'](attr) ** tx
 
         return {'policy': policy_str, 'C0': C0, 'C1': C1, 'C2': C2, 'C3': C3, 'C4': C4}
 
     def decrypt(self, gp, sk, ct):
-        hgid = gp['H'](sk['GID'])
-        policy = util.createPolicy(ct['policy'])
-        coefficients = util.getCoefficients(policy)
-        pruned_list = util.prune(policy, sk['keys'].keys())
+        """
+        Decrypt the ciphertext using the secret keys of the user.
+        :param gp: The global parameters.
+        :param sk: The secret keys of the user.
+        :param ct: The ciphertext to decrypt.
+        :return: The decrypted message.
+        :raise Exception: When the access policy can not be satisfied with the user's attributes.
+        """
+        policy = self.util.createPolicy(ct['policy'])
+        coefficients = self.util.getCoefficients(policy)
+        pruned_list = self.util.prune(policy, sk['keys'].keys())
 
         if not pruned_list:
-            return group.init(GT, 1)
+            raise Exception("You don't have the required attributes for decryption!")
 
-        B = group.init(GT, 1)
+        B = self.group.init(GT, 1)
         for i in range(len(pruned_list)):
             x = pruned_list[i].getAttribute()  # without the underscore
             y = pruned_list[i].getAttributeAndIndex()  # with the underscore
-            B *= (ct['C1'][y] * pair(ct['C2'][y], sk['keys'][x]['K']) * pair(ct['C3'][y], hgid) * pair(
+            B *= (ct['C1'][y] * pair(ct['C2'][y], sk['keys'][x]['K']) * pair(ct['C3'][y], gp['H'](sk['GID'])) * pair(
                 sk['keys'][x]['KP'], ct['C4'][y])) ** coefficients[y]
         return ct['C0'] / B
 
 
-def pretty_print(init_str, my_dict, tab=""):
-    types_enum = ["ZP", "G1", "G2", "GT"]
-    if len(init_str) > 0:
-        print(init_str)
-    for (k, v) in my_dict.items():
-        if isinstance(v, dict):
-            print(tab, k, ": ", type(v))
-            pretty_print("", v, tab + "    ")
-        elif isinstance(v, str):
-            print(tab, k, ": ", v)
-        elif isinstance(v, set):
-            print(tab, k, ": ", v)
-        elif isinstance(v, pairing):
-            print(tab, k, ": ", types_enum[v.type])
-        else:
-            print(tab, k, ": ", type(v))
-    if tab == "":
-        print("\n")
-
-
-def main():
-    pass
-    # curve = 'MNT224'
-    #
-    # groupObj = PairingGroup(curve)
-    # scheme = MAABE_RW12(groupObj)
-    # print("Curve = ", curve)
-    #
-    # ID = InitBenchmark()
-    # startAll(ID)
-    # gp = scheme.GlobalSetup()
-    # EndBenchmark(ID)
-    # boxGS = getResAndClear(ID, "GSetup(" + curve + ")", "Done!")
-    #
-    # # prettyPrint("The global parameters are ", gp)
-    #
-    # pks, sks = {}, {}
-    #
-    # ID = InitBenchmark()
-    # startAll(ID)
-    # (pk, sk) = scheme.AuthSetup(gp, "UT")
-    # EndBenchmark(ID)
-    # boxAS = getResAndClear(ID, "ASetup(" + "UT" + ")", "Done!")
-    #
-    # pks[pk['name']] = pk
-    # sks[sk['name']] = sk
-    #
-    # (pk, sk) = scheme.AuthSetup(gp, "OU")
-    # pks[pk['name']] = pk
-    # sks[sk['name']] = sk
-    #
-    # # prettyPrint("The authority public key chain is ", pks)
-    # # prettyPrint("The authority secret key chain is ", sks)
-    #
-    # ID = InitBenchmark()
-    # startAll(ID)
-    # key = scheme.keygen(gp, "YANNIS", sks, {"STUDENT@UT", "PHD@UT"})
-    # EndBenchmark(ID)
-    # boxKG = getResAndClear(ID, "KeyGen", "Done!")
-    #
-    # # prettyPrint("The secret key is ", key)
-    #
-    # m = scheme.random_message()
-    # policy = '(STUDENT@UT or PROFESSOR@OU) and (STUDENT@UT or MASTERS@OU)'
-    #
-    # ID = InitBenchmark()
-    # startAll(ID)
-    # ct = scheme.encrypt(gp, pks, m, policy)
-    # EndBenchmark(ID)
-    # boxEC = getResAndClear(ID, "Encrypt", "Done!")
-    #
-    # # prettyPrint("The ciphertext is ", ct)
-    #
-    # ID = InitBenchmark()
-    # startAll(ID)
-    # res = scheme.decrypt(gp, key, ct)
-    # EndBenchmark(ID)
-    #
-    # if res == m:
-    #     fin = "Successful Decryption :)"
-    # else:
-    #     fin = "Failed Decryption :("
-    #
-    # boxDE = getResAndClear(ID, "Decrypt", fin)
-    #
-    # # print(fin)
-    #
-    # print(formatNice(boxGS, boxAS, boxKG, boxEC, boxDE))
-
-
 if __name__ == '__main__':
-    debug = True
-    main()
+    import doctest
+    doctest.testmod()
