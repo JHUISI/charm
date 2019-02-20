84ad81491012b39bd507a92720e11eacd94d44b1
Douglas Hellinger <doug.hellinger@hotmail.com>
2019-02-20 20:37:45 +0800

    fix path to policy expression tests.

--- conftest.py
+++ conftest.py
@@ -3,4 +3,4 @@ import sys
 collect_ignore = []
 if sys.version_info < (3, 4):
     collect_ignore.append("charm/toolbox/policy_expression_spec.py")
-    collect_ignore.append("charm/toolbox/test_policy_expression.py")
+    collect_ignore.append("charm/test/toolbox/test_policy_expression.py")
