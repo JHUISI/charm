239763e8123798e436a577b56023f835264d7717
Douglas Hellinger <doug.hellinger@hotmail.com>
2019-02-20 18:04:32 +0800

    skip YLLC15 tests for python sys.version < 3.4

--- charm/test/schemes/abenc/abenc_yllc15_test.py
+++ charm/test/schemes/abenc/abenc_yllc15_test.py
@@ -1,5 +1,7 @@
+import sys
 import unittest
 
+import pytest
 from hypothesis import given
 from hypothesis.strategies import lists
 
@@ -18,6 +20,8 @@ class YLLC15Test(unittest.TestCase):
     def test_ukgen(self, user_id='bob@example.com'):
         (public_key, secret_key) = self.abe.ukgen(self.params, user_id)
 
+    @pytest.mark.skipif(sys.version_info < (3, 4),
+                        reason="requires python3.4 or higher")
     @given(attrs=lists(attributes(), min_size=1))
     def test_proxy_key_gen(self, attrs):
         pkcs, skcs = self.abe.ukgen(self.params, "aws@amazonaws.com")
@@ -39,6 +43,8 @@ class YLLC15Test(unittest.TestCase):
         recovered_key_elem = self.abe.decrypt(self.params, sku, intermediate_value)
         self.assertEqual(random_key_elem, recovered_key_elem)
 
+    @pytest.mark.skipif(sys.version_info < (3, 4),
+                        reason="requires python3.4 or higher")
     @given(policy=policy_expressions())
     def test_policy_not_satisfied(self, policy):
         pkcs, skcs = self.abe.ukgen(self.params, "aws@amazonaws.com")
