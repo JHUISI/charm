0d6111fc794a88bb067529230766f228af5ef344
Douglas Hellinger <doug.hellinger@hotmail.com>
2019-05-18 23:26:40 +0800

    Exclude `abenc_yllc15_bench.py` from python <3.4 version.

--- conftest.py
+++ conftest.py
@@ -4,3 +4,4 @@ collect_ignore = []
 if sys.version_info < (3, 4):
     collect_ignore.append("charm/toolbox/policy_expression_spec.py")
     collect_ignore.append("charm/test/toolbox/test_policy_expression.py")
+    collect_ignore.append("charm/test/benchmark/abenc_yllc15_bench.py")
