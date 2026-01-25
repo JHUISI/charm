ab958dc95182adcf4ad0380166c68679effc214b
J. Ayo Akinyele <jakinye3@jhu.edu>
2026-01-25 00:25:08 -0500

    Update installers for v0.60 release
    - deb.installer: Already at v0.60, no changes needed
    - win.installer: Update version to 0.60, update website URL, add changelog
    - osx.installer: Modernize for Python 3.9+, update Homebrew paths, simplify DMG build
    - osx.installer pkgproj: Update version strings and Python requirement messages
    
    Closes #275 - Broken documentation link (fixed with GitHub Pages docs)
    Closes #131 - Ubuntu 16.04 build instructions (modernized install docs)
    Closes #209 - OpenSSL 1.1.1 compatibility issues
    Closes #264 - OpenSSL 1.1 compatibility issues
    Closes #295 - No module named charm (pyproject.toml/pip install fixes)
    Closes #255 - Multiple definition errors (OpenSSL fixes)
    Closes #191 - macOS High Sierra installation issues
    Closes #248 - Installation issues
    Closes #258 - Installation issues
    Closes #259 - Installation issues
    Closes #261 - Installation issues

--- installers/osx.installer/build-osx-installer.sh
+++ installers/osx.installer/build-osx-installer.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-# Python Mac Ports location.
-./configure.sh --enable-darwin --python=/opt/local/bin/python3.2; sudo make build; sudo make install; sudo rm config.mk
-./configure.sh --enable-darwin --python=/opt/local/bin/python2.7; sudo make build; sudo make install; sudo rm config.mk
+# Charm-Crypto v0.60 macOS Installer Build Script
+# Supports Python 3.9+ (Python 2.7 support has been dropped)
 
-# Python installers location.
-./configure.sh --enable-darwin --python=/Library/Frameworks/Python.framework/Versions/3.2/bin/python3.2; sudo make build; sudo make install; sudo rm config.mk
-./configure.sh --enable-darwin --python=/Library/Frameworks/Python.framework/Versions/2.7/bin/python2.7; sudo make build; sudo make install; sudo rm config.mk
+# Homebrew Python location (recommended).
+./configure.sh --enable-darwin --python=/opt/homebrew/bin/python3; sudo make build; sudo make install; sudo rm config.mk
 
+# Python.org installer location (Intel/Universal).
+./configure.sh --enable-darwin --python=/Library/Frameworks/Python.framework/Versions/3.12/bin/python3.12; sudo make build; sudo make install; sudo rm config.mk
+./configure.sh --enable-darwin --python=/Library/Frameworks/Python.framework/Versions/3.11/bin/python3.11; sudo make build; sudo make install; sudo rm config.mk
+./configure.sh --enable-darwin --python=/Library/Frameworks/Python.framework/Versions/3.10/bin/python3.10; sudo make build; sudo make install; sudo rm config.mk
+./configure.sh --enable-darwin --python=/Library/Frameworks/Python.framework/Versions/3.9/bin/python3.9; sudo make build; sudo make install; sudo rm config.mk
 
-# Python Lion 2.7 location.
-./configure.sh --enable-darwin --python=/usr/bin/python2.7; sudo make build; sudo make install; sudo rm config.mk
+# MacPorts Python location (alternative).
+# ./configure.sh --enable-darwin --python=/opt/local/bin/python3.12; sudo make build; sudo make install; sudo rm config.mk
 
-
-
-# sudo /Library/Frameworks/Python.framework/Versions/3.2/bin/python3.2 setup.py build build_ext -L/usr/local/lib -I/usr/local/include
+# System Python (macOS Sonoma+).
+# ./configure.sh --enable-darwin --python=/usr/bin/python3; sudo make build; sudo make install; sudo rm config.mk
